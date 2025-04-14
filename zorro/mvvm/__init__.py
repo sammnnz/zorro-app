1d5983c2edc3bd13aeff3598118a3f14f22dfae9
sammnnz <104736093+sammnnz@users.noreply.github.com>
2025-04-14 04:24:07 +0300

    Add special routes to `Node.js` (jquery, jquery-ui)

--- zorro/mvvm/__init__.py
+++ zorro/mvvm/__init__.py
@@ -10,25 +10,51 @@ __all__.extend(bindings.__all__)
 __all__.extend(core.__all__)
 __all__.extend(interfaces.__all__)
 
-_JS_FILES = ('binder.js', 'bindings.js', 'notify.js')
+_files = ('binder.js', 'bindings.js', 'jquery.js', 'jquery-ui.js', 'jquery-ui.css', 'notify.js')
 
 
 def init():
     from bottle import route
 
     route(path="/mvvm/js/<file:path>", callback=_file_js)
+    route(path="/node_modules/js/<file:path>", callback=_node_js)
+    route(path="/node_modules/css/<file:path>", callback=_node_js)
 
 
 def _file_js(file: str):
     from bottle import static_file
     from eel import _set_response_headers
 
-    responce = ''  # TODO: выяснить что отобразится в html
-    if file not in _JS_FILES:
+    responce = ''
+    if file not in _files:
         return responce
 
     response = static_file(os.path.join('js', file), root=Path(__file__).parent)
     response.content_type = 'application/javascript'
+
+    _set_response_headers(response)
+    return response
+
+
+def _node_js(file: str):
+    from bottle import static_file
+    from eel import _set_response_headers
+
+    responce = ''
+    if file not in _files:
+        return responce
+
+    path, content_type = '', ''
+    if file.endswith('.js'):
+        path = os.path.join('node_modules', file.removesuffix('.js'), 'dist', file)
+        content_type = 'application/javascript'
+    elif file.endswith('.css'):
+        path = os.path.join('node_modules', file.removesuffix('.css'), 'dist', 'themes', 'base', file)
+        content_type = 'text/css'
+
+    response = static_file(path, root=os.getcwd())
+    response.content_type = content_type
+    print('\n', file, '\n')
 
     _set_response_headers(response)
     return response
